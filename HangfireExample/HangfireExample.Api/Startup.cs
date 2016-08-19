123806727ba2b2c366faddd9afe590852c11642e
mblanchard <mblanchard@CHIMBLANCHARD>
2016-08-19 11:45:35 -0500

    - added mock failing job - added second backgroundJobServer

--- HangfireExample/HangfireExample.Api/Startup.cs
+++ HangfireExample/HangfireExample.Api/Startup.cs
@@ -17,14 +17,23 @@ namespace HangfireExample.Api
         {
             ConfigureAuth(app);
             GlobalConfiguration.Configuration.UseSqlServerStorage("DefaultConnection", new SqlServerStorageOptions { QueuePollInterval = TimeSpan.FromSeconds(1) });
-            BackgroundJob.Enqueue(() => Console.WriteLine("Fire-and-forget"));
-            BackgroundJob.Schedule(() => Console.WriteLine("Delayed"), TimeSpan.FromSeconds(30));
-            RecurringJob.AddOrUpdate(() => Console.WriteLine("Every Minute"), Cron.Minutely);
-            RecurringJob.AddOrUpdate(() => RequestProducer.CreateNewRequests(), Cron.MinuteInterval(5));
-            RecurringJob.AddOrUpdate(() => RequestConsumer.ProcessRequests(), Cron.Minutely);
+            BackgroundJob.Enqueue(() => Console.WriteLine("A fire-and-forget job that runs on app start"));
+            BackgroundJob.Schedule(() => Console.WriteLine("A fire-and-forget job that runs once after 30 seconds"), TimeSpan.FromSeconds(30));
+            RecurringJob.AddOrUpdate("Minutely",() => Console.WriteLine("Every Minute"), Cron.Minutely);
+            RecurringJob.AddOrUpdate("Request Producer",() => RequestProducer.CreateNewRequests(), Cron.MinuteInterval(5));
+            RecurringJob.AddOrUpdate("Request Consumer",() => RequestConsumer.ProcessRequests(), Cron.Minutely);
+
+            RecurringJob.AddOrUpdate("Mock job that fails half the time", () => MockJobs.RunWithTransientErrors(), Cron.Minutely);
+
+            var server = new BackgroundJobServer(GenerateServerOptions());
 
             app.UseHangfireDashboard();
-            app.UseHangfireServer();
+            app.UseHangfireServer(GenerateServerOptions());
+        }
+
+        private BackgroundJobServerOptions GenerateServerOptions()
+        {
+            return new BackgroundJobServerOptions { ServerName = String.Format("{0}.{1}", Environment.MachineName, Guid.NewGuid().ToString()) };
         }
     }
 }
