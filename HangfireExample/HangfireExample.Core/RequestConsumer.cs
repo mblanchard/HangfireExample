744273582119f9aa6d4e222312f493d5e06f6b81
Matthew Blanchard <mblanchard@us.costar.local>
2016-08-19 09:42:49 -0500

    dispose of dbcontext

--- HangfireExample/HangfireExample.Core/RequestConsumer.cs
+++ HangfireExample/HangfireExample.Core/RequestConsumer.cs
@@ -9,16 +9,14 @@ namespace HangfireExample.Core
     {
         public static void ProcessRequests()
         {
-            Entities _context;
-            var now = DateTime.Now;
-            _context = new Entities(); // TODO: DI
-            var nextReq = _context.Requests.Where(x => x.Status == 0).FirstOrDefault();
-            if (nextReq == null) return;
-            nextReq.Status = 1;
-            _context.Entry(nextReq).State = System.Data.Entity.EntityState.Modified;
-            _context.SaveChanges();
-
-
+            using (Entities _context = new Entities())
+            {
+                var nextReq = _context.Requests.Where(x => x.Status == 0).FirstOrDefault();
+                if (nextReq == null) return;
+                nextReq.Status = 1;
+                _context.Entry(nextReq).State = System.Data.Entity.EntityState.Modified;
+                _context.SaveChanges();
+            }
         }
     }
 }
--- HangfireExample/HangfireExample.Core/RequestProducer.cs
+++ HangfireExample/HangfireExample.Core/RequestProducer.cs
@@ -12,11 +12,12 @@ namespace HangfireExample.Core
 
         public static void CreateNewRequests()
         {
-            Entities _context;
-            var now = DateTime.Now;
-            _context = new Entities(); // TODO: DI
-            _context.Requests.AddRange(Enumerable.Range(0, new Random().Next(10)).Select(x => new Request() { Text = String.Format("Task: {0}-{1}", now, x), Key = String.Format("{0}-{1}", now, x), Status=0, IsVisible=true }).ToList());
-            _context.SaveChanges();
+            using (Entities _context = new Entities())
+            {
+                var now = DateTime.Now;
+                _context.Requests.AddRange(Enumerable.Range(0, new Random().Next(10)).Select(x => new Request() { Text = String.Format("Task: {0}-{1}", now, x), Key = String.Format("{0}-{1}", now, x), Status = 0, IsVisible = true }).ToList());
+                _context.SaveChanges();
+            }
         }
 
     }
